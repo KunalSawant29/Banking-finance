873a7a1fa5ca004ae6c9772631ad18922700f4ed
KunalSawant29 <137906303+KunalSawant29@users.noreply.github.com>
2023-09-22 21:50:33 +0530

    Update main.tf

--- my-serverfiles/main.tf
+++ my-serverfiles/main.tf
@@ -1,12 +1,12 @@
 resource "aws_instance" "test-server" {
   ami           = "ami-0f5ee92e2d63afc18" 
   instance_type = "t2.micro" 
-  key_name = "BabucKeypair"
+  key_name = "myownkey"
   vpc_security_group_ids= ["sg-088585bb55f0856c0"]
   connection {
     type     = "ssh"
     user     = "ubuntu"
-    private_key = file("./BabucKeypair.pem")
+    private_key = file("./myownkey.pem")
     host     = self.public_ip
   }
   provisioner "remote-exec" {
@@ -19,6 +19,6 @@ resource "aws_instance" "test-server" {
         command = " echo ${aws_instance.test-server.public_ip} > inventory "
   }
    provisioner "local-exec" {
-  command = "ansible-playbook /var/lib/jenkins/workspace/Banking/my-serverfiles/finance-playbook.yml "
+  command = "ansible-playbook /var/lib/jenkins/workspace/Banking-Finance/my-serverfiles/finance-playbook.yml "
   } 
 }
